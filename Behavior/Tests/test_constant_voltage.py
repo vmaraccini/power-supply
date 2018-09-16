b9e5878c07c43e653ac6c960188baedd38a89628
Victor Maraccini <vgm.maraccini@gmail.com>
2018-09-16 17:52:55 -0300

    Re-implement constant voltage tests with new runner

--- Behavior/Tests/test_constant_voltage.py
+++ Behavior/Tests/test_constant_voltage.py
@@ -6,19 +6,37 @@ import os
 
 
 class TestConstantVoltage(TestCase):
+    v_set = 5
+    max_ripple = 1e-3
+    max_overshoot = 0.2
+    max_settle_time = 5e-3
 
     def test_constant_voltage_mode(self):
         params = {
             "RLoad": "1e12",
-            "Vset": "5",
+            "Vset": str(self.v_set),
             "Iset": "1",
             "Vin": "12",
         }
 
+        waveforms = SimulationRunner().simulate(params=params)
+        output_voltage = waveforms['V(out)']
 
-
-        assert_settles(waveforms['V(out)'],
-                       value=5,
-                       accuracy=1e-3,
+        assert_settles(output_voltage,
+                       value=self.v_set,
+                       accuracy=self.max_ripple,
                        min_duration=1e-3,
-                       before=5e-3)
+                       before=self.max_settle_time)
+
+        assert_bound(output_voltage,
+                     lower=0,
+                     upper=self.v_set + self.max_overshoot)
+
+        settle_start = settle_time(output_voltage,
+                                   value=self.v_set,
+                                   accuracy=self.max_ripple,
+                                   min_duration=1e-3)
+
+        assert_bound(output_voltage[settle_start:],
+                     lower=self.v_set - self.max_ripple,
+                     upper=self.v_set - self.max_ripple)
