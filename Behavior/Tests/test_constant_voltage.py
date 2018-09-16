44a9ee2bf118782cde9db144937aa85bf0644bef
Victor Maraccini <vgm.maraccini@gmail.com>
2018-09-16 17:52:22 -0300

    Refactor simulation setup and running into a reusable class

--- Behavior/Tests/test_constant_voltage.py
+++ Behavior/Tests/test_constant_voltage.py
@@ -1,17 +1,13 @@
 from Utilities.Waveform.waveform import Waveform
-from Utilities.LTspiceCLI.simulate import *
-from Utilities.LTspiceCLI.parse import *
+from simulation_runner import *
 from Utilities.Waveform.waveform_assertions import *
 from unittest import TestCase
 import os
 
 
 class TestConstantVoltage(TestCase):
-    config = {"executable_path": "/Applications/LTspice.app/Contents/MacOS/LTspice"}
-    filename = "../PowerSupply.asc"
 
     def test_constant_voltage_mode(self):
-        simulator = Simulator(**self.config)
         params = {
             "RLoad": "1e12",
             "Vset": "5",
@@ -19,13 +15,7 @@ class TestConstantVoltage(TestCase):
             "Vin": "12",
         }
 
-        dir = os.path.dirname(__file__)
-        result_file = simulator.simulate(os.path.join(dir, self.filename), params=params)
-        parser = Parser()
-        parser.parse(result_file)
 
-        json = parser.serialize()
-        waveforms = Waveform.parse(json)
 
         assert_settles(waveforms['V(out)'],
                        value=5,
