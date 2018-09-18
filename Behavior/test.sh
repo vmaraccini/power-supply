a73470da05935560d5b869862b9e771b45765563
Victor Maraccini <vgm.maraccini@gmail.com>
2018-09-17 22:31:17 -0300

    Update image name

--- Behavior/test.sh
+++ Behavior/test.sh
@@ -1,2 +1,2 @@
 docker pull vmaraccini/ltspice:latest
-cat "docker-test.sh" | docker run -i -v "`pwd`":/mnt/project ltspice 
+cat "docker-test.sh" | docker run -i -v "`pwd`":/mnt/project vmaraccini/ltspice 
