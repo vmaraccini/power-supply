2efc58fecb186039cade7173078f2ad6d8f2c99a
Victor Maraccini <vgm.maraccini@gmail.com>
2018-09-21 21:44:56 -0300

    Allow injection of extra parameters during testing

--- Behavior/test.sh
+++ Behavior/test.sh
@@ -1,4 +1,5 @@
-EXTRA_PARAM=$1
+EXTRA_PARAM=${1:-"discover -s Tests"}
 
+echo "Running tests with arguments: $EXTRA_PARAM"
 docker pull vmaraccini/ltspice:latest
 { cat "docker-test.sh"; echo " $EXTRA_PARAM" ; } | docker run -i -v "$(pwd)":/mnt/project vmaraccini/ltspice 
