beaaa9401140b356b855c69503c417f0eb6e24aa
Victor Maraccini <vgm.maraccini@gmail.com>
2018-10-03 23:20:12 -0300

    Fix internal Waveform tests so they do not generate debug images

--- Behavior/Tests/Utilities/Waveform/Tests/test_waveform_assertions.py
+++ Behavior/Tests/Utilities/Waveform/Tests/test_waveform_assertions.py
@@ -7,33 +7,41 @@ class TestWaveformAssertions(TestCase):
     waveform = Waveform(timestamps=[0, 1, 2, 3, 4], values=[0, 3, 2, 2.2, 1.8])
 
     def test_assert_settles(self):
-        assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=2)
-        assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=2, before=4)
+        assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=2, generate_images=False)
+        assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=2, before=4, generate_images=False)
 
         self.assertRaises(Exception,
-                          lambda: assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=2, before=1))
+                          lambda: assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=2, before=1,
+                                                 generate_images=False))
         self.assertRaises(Exception,
-                          lambda: assert_settles(self.waveform, value=1, accuracy=0.2, min_duration=2, before=3))
+                          lambda: assert_settles(self.waveform, value=1, accuracy=0.2, min_duration=2, before=3,
+                                                 generate_images=False))
         self.assertRaises(Exception,
-                          lambda: assert_settles(self.waveform, value=2, accuracy=0.1, min_duration=2, before=3))
+                          lambda: assert_settles(self.waveform, value=2, accuracy=0.1, min_duration=2, before=3,
+                                                 generate_images=False))
         self.assertRaises(Exception,
-                          lambda: assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=3, before=3))
+                          lambda: assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=3, before=3,
+                                                 generate_images=False))
         self.assertRaises(Exception,
-                          lambda: assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=2, before=1))
+                          lambda: assert_settles(self.waveform, value=2, accuracy=0.2, min_duration=2, before=1,
+                                                 generate_images=False))
 
     def test_assert_bound(self):
-        assert_bound(self.waveform, lower=0)
-        assert_bound(self.waveform, upper=3)
-        assert_bound(self.waveform, lower=0, upper=3)
-        assert_bound(self.waveform, lower=1, ignore_before=1)
-        self.assertRaises(Exception, lambda: assert_bound(self.waveform, lower=1, ignore_before=None))
-        self.assertRaises(Exception, lambda: assert_bound(self.waveform, upper=2, ignore_before=None))
+        assert_bound(self.waveform, lower=0, generate_images=False)
+        assert_bound(self.waveform, upper=3, generate_images=False)
+        assert_bound(self.waveform, lower=0, upper=3, generate_images=False)
+        assert_bound(self.waveform, lower=1, ignore_before=1, generate_images=False)
+        self.assertRaises(Exception,
+                          lambda: assert_bound(self.waveform, lower=1, ignore_before=None, generate_images=False))
+        self.assertRaises(Exception,
+                          lambda: assert_bound(self.waveform, upper=2, ignore_before=None, generate_images=False))
 
     def test_assert_in_range(self):
         assert_in_range(self.waveform, (0, 3))
         assert_in_range(self.waveform, (1, 5), ignore_before=1)
         self.assertRaises(Exception, lambda: assert_in_range(self.waveform, (0, 2)))
-        self.assertRaises(Exception, lambda: assert_in_range(self.waveform, (1, 5)), ignore_before=None)
+        self.assertRaises(Exception, lambda: assert_in_range(self.waveform, (1, 5)), ignore_before=None,
+                          generate_images=False)
 
     def test_settle_time(self):
         waveform = Waveform(timestamps=[0, 1, 2, 3, 4, 5, 6, 7], values=[0, 3, 2, 2.2, 1.8, 2.1, 1.9, 2])
