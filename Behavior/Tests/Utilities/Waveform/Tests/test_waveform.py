eec110488e007b0a06667320d4a6f034e6e3be4e
Victor Maraccini <vgm.maraccini@gmail.com>
2018-10-07 22:25:59 -0300

    Add mult., add, sub, div, equatable and abs operators to Waveform

--- Behavior/Tests/Utilities/Waveform/Tests/test_waveform.py
+++ Behavior/Tests/Utilities/Waveform/Tests/test_waveform.py
@@ -54,3 +54,85 @@ class TestWaveform(TestCase):
         self.assertEqual(len(test_waveform), 13601)
         self.assertAlmostEqual(test_waveform[0], 0, places=6)
         self.assertAlmostEqual(test_waveform[1e-3], 4, places=6)
+
+    def test_add(self):
+        one = Waveform([0, 2, 4], [0, 2, 4])
+        other = Waveform([0, 2, 4], [1, 3, 5])
+
+        result = one + other
+        self.assertEqual(result.timestamps, one.timestamps)
+        self.assertEqual(result.values, [1, 5, 9])
+
+    def test_add_interpolated(self):
+        one = Waveform([0, 2, 4, 6], [0, 2, 4, 6])
+        other = Waveform([1, 3, 5], [1, 3, 5])
+
+        result = one + other
+        self.assertEqual(result.timestamps, one.timestamps)
+        map(lambda (x, y): self.assertAlmostEqual(x, y, places=6), zip(result.values, [1, 4, 8, 11]))
+
+    def test_sub(self):
+        one = Waveform([0, 2, 4], [0, 2, 4])
+        other = Waveform([0, 2, 4], [1, 3, 5])
+
+        result = one - other
+        self.assertEqual(result.timestamps, one.timestamps)
+        self.assertEqual(result.values, [-1, -1, -1])
+
+    def test_sub_interpolated(self):
+        one = Waveform([0, 2, 4, 6], [0, 2, 4, 6])
+        other = Waveform([1, 3, 5], [1, 3, 5])
+
+        result = one - other
+        self.assertEqual(result.timestamps, one.timestamps)
+        map(lambda (x, y): self.assertAlmostEqual(x, y, places=6), zip(result.values, [-1, 0, 0, 1]))
+
+    def test_mul(self):
+        one = Waveform([0, 2, 4], [0, 2, 4])
+        other = Waveform([0, 2, 4], [1, 3, 5])
+
+        result = one * other
+        self.assertEqual(result.timestamps, one.timestamps)
+        self.assertEqual(result.values, [0, 6, 20])
+
+    def test_mul_scalar(self):
+        result = 2.0 * self.test_waveform
+        self.assertEqual(result.timestamps, self.test_waveform.timestamps)
+        self.assertAlmostEqual(result.values, [8, 6, 4, 2], places=6)
+
+        result = 2.0 * self.test_waveform
+        self.assertEqual(self.test_waveform * 2, result)
+
+    def test_mul_interpolated(self):
+        one = Waveform([0, 2, 4, 6], [0, 2, 4, 6])
+        other = Waveform([1, 3, 5], [1, 3, 5])
+
+        result = one * other
+        self.assertEqual(result.timestamps, one.timestamps)
+        map(lambda (x, y): self.assertAlmostEqual(x, y, places=6), zip(result.values, [0, 4, 16, 30]))
+
+    def test_div(self):
+        one = Waveform([0, 2, 4], [0, 2, 4])
+        other = Waveform([0, 2, 4], [1, 3, 5])
+
+        result = one / other
+        self.assertEqual(result.timestamps, one.timestamps)
+        self.assertEqual(result.values, [0, 2.0 / 3, 4.0 / 5])
+
+    def test_div_scalar(self):
+        result = 2.0 / self.test_waveform
+        self.assertEqual(result.timestamps, self.test_waveform.timestamps)
+        map(lambda (x, y): self.assertAlmostEqual(x, y, places=6), zip(result.values, [0.5, 2.0 / 3, 1, 2]))
+
+    def test_rdiv_scalar(self):
+        result = self.test_waveform / 0.5
+        self.assertEqual(result.timestamps, self.test_waveform.timestamps)
+        self.assertEqual(result.values, [8, 6, 4, 2])
+
+    def test_div_interpolated(self):
+        one = Waveform([0, 2, 4, 6], [0, 2, 4, 6])
+        other = Waveform([1, 3, 5], [1, 3, 5])
+
+        result = one / other
+        self.assertEqual(result.timestamps, one.timestamps)
+        map(lambda (x, y): self.assertAlmostEqual(x, y, places=6), zip(result.values, [0, 1, 1, 6.0 / 5]))
--- Behavior/Tests/Utilities/Waveform/waveform.py
+++ Behavior/Tests/Utilities/Waveform/waveform.py
@@ -115,8 +115,67 @@ class Waveform:
 
             return initial + (final - initial) * progress
 
+    def __mul__(self, other):
+        if isinstance(other, (int, long, float)):
+            return Waveform(self.timestamps, [other * x for x in self.values])
+        if isinstance(other, Waveform):
+            if other.timestamps == self.timestamps:
+                other_values = other.values
+            else:
+                other_values = map(lambda x: other[x], self.timestamps)
+            return Waveform(self.timestamps, [x * y for (x, y) in zip(other_values, self.values)])
+        else:
+            raise Exception("Unimplemented multiplication between Waveform and {}".format(type(other)))
+
+    __rmul__ = __mul__
+
+    def __div__(self, other):
+        if isinstance(other, (int, long, float)):
+            return Waveform(self.timestamps, [float(x) / other for x in self.values])
+        if isinstance(other, Waveform):
+            if other.timestamps == self.timestamps:
+                other_values = other.values
+            else:
+                other_values = map(lambda x: other[x], self.timestamps)
+            return Waveform(self.timestamps, [float(x) / y for (x, y) in zip(self.values, other_values)])
+        else:
+            raise Exception("Unimplemented division between Waveform and {}".format(type(other)))
+
+    def __rdiv__(self, other):
+        if isinstance(other, (int, long, float)):
+            return Waveform(self.timestamps, [other / float(x) for x in self.values])
+        if isinstance(other, Waveform):
+            if other.timestamps == self.timestamps:
+                other_values = other.values
+            else:
+                other_values = map(lambda x: other[x], self.timestamps)
+            return Waveform(self.timestamps, [float(x) / y for (x, y) in zip(self.values, other_values)])
+        else:
+            raise Exception("Unimplemented division between Waveform and {}".format(type(other)))
+
+    def __add__(self, other):
+        if isinstance(other, Waveform):
+            if other.timestamps == self.timestamps:
+                other_values = other.values
+            else:
+                other_values = map(lambda x: other[x], self.timestamps)
+            return Waveform(self.timestamps, [sum(x) for x in zip(other_values, self.values)])
+        else:
+            raise Exception("Unimplemented add between Waveform and {}".format(type(other)))
+
+    def __sub__(self, other):
+        return self + Waveform(other.timestamps, [-x for x in other.values])
+
     def __len__(self):
         return len(self.timestamps)
+
+    def __eq__(self, other):
+        if isinstance(other, Waveform):
+            return other.values == self.values and other.timestamps == self.timestamps
+        return False
+
+    def __abs__(self):
+        return Waveform(self.timestamps, [abs(x) for x in self.values])
 
     def __getitem__(self, item):
         if isinstance(item, (int, long, float)):
